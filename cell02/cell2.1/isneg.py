9b0da310d491546fbf3a368ec8d4476f103091bf
ice6bath <icebathopenai@gmail.com>
2025-03-10 14:27:05 +0700

    Add files via upload

---
+++ cell02/cell2.1/isneg.py
@@ -0,0 +1,7 @@
+X = int(input("number:"))
+if X > 0 :
+    print("This number is positive.")
+elif X < 0 :
+    print("This number is negative.")
+else:
+    print("This number is both positive and negative.") 
