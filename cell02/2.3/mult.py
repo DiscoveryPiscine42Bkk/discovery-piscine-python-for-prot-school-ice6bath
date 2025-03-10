1c8ba8714608738fab191fe115daaba0ed97da6a
ice6bath <icebathopenai@gmail.com>
2025-03-10 15:11:32 +0700

    Add files via upload

---
+++ cell02/2.3/mult.py
@@ -0,0 +1,9 @@
+X = int(input("Enter your fist number:"))
+Y = int(input("Enter your second number:"))
+print(X * Y)
+if X * Y > 0 :
+    print("The result is positive.")
+elif X * Y < 0 :
+    print("The result is Negative.")
+else :
+    print("The result is positive and negative.")
